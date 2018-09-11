d12ce18b9d801b4b596c1ebdc2280b19375f71b8
rhaughey <rfhaughey@gmail.com>
2018-09-11 18:09:43 -0400

    Reactive WebClient

--- src/main/java/guru/springframework/springrestclientexamples/controllers/UserController.java
+++ src/main/java/guru/springframework/springrestclientexamples/controllers/UserController.java
@@ -4,7 +4,6 @@ import guru.springframework.springrestclientexamples.services.ApiService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.server.ServerWebExchange;
@@ -27,19 +26,11 @@ public class UserController {
     @PostMapping("/users")
     public String formPost(Model model, ServerWebExchange serverWebExchange) {
 
-        MultiValueMap<String, String> map = serverWebExchange.getFormData().block();
-
-        Integer limit = new Integer(map.get("limit").get(0));
-
-        log.debug("Received Limit Value: " + limit);
-
-        //default to 10 if null or zero
-        if (limit == null || limit == 0){
-            log.debug("Setting limit to default of 10");
-            limit = 10;
-        }
-
-        model.addAttribute("users", apiService.getUsers(limit));
+        model.addAttribute("users",
+                apiService
+                        .getUsers(serverWebExchange
+                            .getFormData()
+                            .map(data -> new Integer(data.getFirst("limit")))));
 
         return "userlist";
     }
